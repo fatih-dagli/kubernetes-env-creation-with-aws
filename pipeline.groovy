50689ffba7b02f2a8c73474b9886a56601224966
Fatih Dagli (A) <f00598599@china.huawei.com>
2022-04-27 11:25:23 +0300

    added python deployment yamls

--- pipeline.groovy
+++ pipeline.groovy
@@ -25,9 +25,31 @@ node {
             sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@xxx.xxx.xxx.xxx 'kubectl apply -f /mysql.yml'"
         }
     }
+
+    stage('set aws_access_key_id') {
+        dir("/var/lib/jenkins/workspace/initial-deploy/kubernetes") {
+            sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@xxx.xxx.xxx.xxx 'aws configure set aws_access_key_id AKIAYB5PTDSDCC35M3V6'"
+            sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@yyy.yyy.yyy.yyy 'aws configure set aws_access_key_id AKIAYB5PTDSDCC35M3V6'"
+        }
+    }
+
+    stage('set aws_secret_access_key') {
+        dir("/var/lib/jenkins/workspace/initial-deploy/kubernetes") {
+            sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@xxx.xxx.xxx.xxx 'aws configure set aws_secret_access_key NnGo6leNdzoN/LYG6c39itTBAuDF0+750dAvqtql'"
+            sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@yyy.yyy.yyy.yyy 'aws configure set aws_secret_access_key NnGo6leNdzoN/LYG6c39itTBAuDF0+750dAvqtql'"
+        }
+    }
+
+    stage('login to aws registry') {
+        dir("/var/lib/jenkins/workspace/initial-deploy/kubernetes") {
+            sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@xxx.xxx.xxx.xxx 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 553882164358.dkr.ecr.us-east-1.amazonaws.com'"
+            sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@yyy.yyy.yyy.yyy 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 553882164358.dkr.ecr.us-east-1.amazonaws.com'"
+        }
+    }
     
     stage('Build python') {
         dir("/var/lib/jenkins/workspace/initial-deploy/kubernetes") {
+            sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@xxx.xxx.xxx.xxx 'kubectl create secret docker-registry regcred --docker-server=553882164358.dkr.ecr.us-east-1.amazonaws.com --docker-username=AWS --docker-password=\$(aws ecr get-login-password --region us-east-1)'"
             sh "sshpass -p 'Operation123' ssh -o StrictHostKeyChecking=no root@xxx.xxx.xxx.xxx 'kubectl apply -f /python.yml'"
         }
     }
